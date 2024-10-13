3a10de81c63470a8458b160f5e1780b942290066
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-13 12:44:13 +0300

    Update Quiz.jsx

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+import QUESTIONS from "../questions.js";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  //* deriving the indix of the current active question from the questions answered already
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  };
+
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Quiz;
