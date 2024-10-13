97b4f558dda8afc76ccf838470eb6f152fe53a73
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-13 14:48:11 +0300

    Update QuestionTimer.jsx

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const QuestionTimer = () => {
-  return <div>QuestionTimer</div>;
+const QuestionTimer = ({ timeout, onTimeout }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const progressTime = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(progressTime);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 100);
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
 };
 
 export default QuestionTimer;
