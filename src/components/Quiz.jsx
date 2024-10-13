5ad704a5b646ca4ca59ba12603622711d4504e6b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-13 12:47:45 +0300

    Update Quiz.jsx
    styling purposes

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -15,15 +15,19 @@ const Quiz = () => {
   };
 
   return (
-    <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
