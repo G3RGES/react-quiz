0f59925d8e6ada9a3af72e92fee26a3acc523061
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-13 12:44:11 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import quizLogo from "../assets/quiz-logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <img src={quizLogo} alt="ReactQuiz Logo" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
 };
 
 export default Header;
