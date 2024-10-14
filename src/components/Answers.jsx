bbfcf2dd5af282a9955994cbcf1adcca1242b26e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-14 13:18:25 +0300

    Update Answers.jsx

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const Answers = () => {
-  return <div>Answers</div>;
+const Answers = ({ answers, selectedAnswers, answerState, onSelect }) => {
+  const shuffledAnswers = useRef();
+
+  if (!shuffledAnswers.current) {
+    shuffledAnswers.current = [...answers];
+    //* return -1 (negative) shuffles the answers (sort), return 1 (positive) returns the array as it is
+    shuffledAnswers.current.sort(() => Math.random() - 0.5);
+  }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswers === answer;
+        let cssClass = "";
+
+        if (answerState === "answered" && isSelected) {
+          cssClass = "selected";
+        }
+
+        if (
+          isSelected &&
+          (answerState === "correct" || answerState === "wrong")
+        ) {
+          cssClass = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button onClick={() => onSelect(answer)} className={cssClass}>
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Answers;
