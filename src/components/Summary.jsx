34dad1adc77d4a5cbe143d32170ef8c92a094dd8
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-10-14 18:05:51 +0300

    Update Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 
-const Summary = () => {
-  return <div>Summary</div>;
+import quizCompleteLogo from "../assets/quiz-complete.png";
+import QUESTIONS from "../questions.js";
+
+const Summary = ({ userAnswers }) => {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, idx) => answer === QUESTIONS[idx].answers[0]
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswersShare = 100 - correctAnswersShare - skippedAnswersShare;
+
+  return (
+    <div id="summary">
+      <img src={quizCompleteLogo} alt="quiz complete logo" />
+      <h2>Quiz Complete</h2>
+      <div id="summary-stats">
+        <p>
+          <span className="number">{skippedAnswersShare}%</span>
+          <span className="text">Skipped</span>
+        </p>
+        <p>
+          <span className="number">{correctAnswersShare}%</span>
+          <span className="text">Answered Correct</span>
+        </p>
+        <p>
+          <span className="number">{wrongAnswersShare}%</span>
+          <span className="text">Answered Wrong</span>
+        </p>
+      </div>
+      <ol>
+        {userAnswers.map((answer, idx) => {
+          let cssClass = "user-answer";
+
+          if (answer === null) {
+            cssClass += " skipped";
+          } else if (answer === QUESTIONS[idx].answers[0]) {
+            cssClass += " correct";
+          } else {
+            cssClass += " wrong";
+          }
+
+          return (
+            <li key={idx}>
+              <h3>{idx + 1}</h3>
+              <p className="question">{QUESTIONS[idx].text}</p>
+              <p className={cssClass}>
+                {answer ?? "You didn't pick an answer"}
+              </p>
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
 };
 
 export default Summary;
